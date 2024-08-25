2284ea43fe88c5215fe89ad66fbb20d7ed8ca1f4
tanasatit <tanasatit.n@ku.th>
2024-08-25 22:05:12 +0700

    add docstring

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,20 @@
+"""
+Models for the Polls app.
+Defines Question and Choice models.
+"""
+
 import datetime
 from django.db import models
 from django.utils import timezone
 
 
 class Question(models.Model):
+    """A poll question."""
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
+        """Check if the question was published within the last day."""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
@@ -16,10 +23,10 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    """A choice for a poll question."""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
-
