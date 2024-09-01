3e0721ff1be84ab1ca5cbadcddf06b5951eee6f9
tanasatit <tanasatit.n@ku.th>
2024-09-01 20:38:46 +0700

    Navigation Improvements

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.views.generic.base import RedirectView
 from . import views
 
 app_name = 'polls'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
