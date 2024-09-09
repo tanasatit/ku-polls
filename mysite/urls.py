a9e255836da1470ec4967767abf87d8316bdaf20
tanasatit <tanasatit.n@ku.th>
2024-09-09 11:33:17 +0700

    add sign-in page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+from mysite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='/polls/', permanent=False)),  # Redirect base URL to polls index
     path('polls/', include('polls.urls')),  # Include polls app URLs
+    path('signup/', views.signup, name='signup'),
 ]
