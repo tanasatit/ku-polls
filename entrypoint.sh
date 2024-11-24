cd9f0e7570bd4c1353bee1fbeb6c54bcc7e59bb3
tanasatit <tanasatit.n@ku.th>
2024-11-24 20:26:35 +0700

    Add a Health Check for the Database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -e
+
+echo "Waiting for database..."
+while ! nc -z "$DATABASE_HOST" "$DATABASE_PORT"; do
+  sleep 1
+done
+echo "Database is ready!"
+
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
